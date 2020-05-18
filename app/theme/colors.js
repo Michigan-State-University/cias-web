11f2c838820706daa1f245eaea31fe305103b7e2
Michal Grzelak <grzelakm@outlook.com>
2020-05-18 13:35:42 +0200

    Add theme variables

---
+++ app/theme/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  primary: '#23B7A0',
+  secondary: '#C866EA',
+  text: '#2F3850',
+  highlight: '#E9F1FB',
+  white: '#FFF',
+};
+
+export { colors };
---
+++ app/theme/general.js
@@ -0,0 +1,11 @@
+const borders = {
+  borderRadius: '10px',
+  borderStyle: 'solid',
+  borderWidth: '1px',
+};
+
+const paddings = {
+  regular: '16px',
+};
+
+export { borders, paddings };
