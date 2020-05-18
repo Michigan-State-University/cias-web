5c14df8b42126461daf29fa9da10aee21a2f4063
Michal Grzelak <grzelakm@outlook.com>
2020-05-18 09:46:50 +0200

    Add and configure redux-persist

--- app/reducers.js
+++ app/reducers.js
@@ -2,17 +2,22 @@
  * Combine all reducers in this file and export the combined reducers.
  */
 
-import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
+import { persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import history from 'utils/history';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 /**
  * Merges the main reducer with the router state and dynamically injected reducers
  */
 export default function createReducer(injectedReducers = {}) {
-  const rootReducer = combineReducers({
+  const rootReducer = persistCombineReducers(persistConfig, {
     language: languageProviderReducer,
     router: connectRouter(history),
     ...injectedReducers,
