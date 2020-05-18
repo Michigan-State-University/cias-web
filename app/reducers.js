a18c10ac4a5480ec7804dfe429147341bd1ea3c7
Michal Grzelak <grzelakm@outlook.com>
2020-05-18 10:34:50 +0200

    Add 'router' state to blacklist in react-persist

--- app/reducers.js
+++ app/reducers.js
@@ -1,18 +1,19 @@
 /**
  * Combine all reducers in this file and export the combined reducers.
  */
-
-import { connectRouter } from 'connected-react-router';
 import { persistCombineReducers } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+import { connectRouter } from 'connected-react-router';
 import history from 'utils/history';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
 
 const persistConfig = {
   key: 'root',
   storage,
+  blacklist: ['router'],
 };
+
 /**
  * Merges the main reducer with the router state and dynamically injected reducers
  */
