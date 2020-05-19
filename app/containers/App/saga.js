6f19df1d37683808335e89a6f906f39aa7727002
Michal Grzelak <grzelakm@outlook.com>
2020-05-19 11:57:38 +0200

    Redirect to home after login

--- app/containers/App/saga.js
+++ app/containers/App/saga.js
@@ -1,9 +1,11 @@
 import { put, takeEvery, all } from 'redux-saga/effects';
+import { push } from 'connected-react-router';
 import { setToken } from '../../global/reducers/auth';
 import { LOGIN_SUCCESS } from '../LoginPage/constants';
 
 function* tokenAssign({ payload: { token } }) {
   yield put(setToken(token));
+  yield put(push('/'));
 }
 
 export default function* rootSaga() {
