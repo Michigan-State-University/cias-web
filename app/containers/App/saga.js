2856d43f17fee309ec0d5b1be77f167b89197fab
Michal Grzelak <grzelakm@outlook.com>
2020-05-19 12:40:25 +0200

    Set token for every '*_SUCCESS' action

--- app/containers/App/saga.js
+++ app/containers/App/saga.js
@@ -1,7 +1,6 @@
 import { put, takeEvery, all } from 'redux-saga/effects';
 import { push } from 'connected-react-router';
 import { setToken } from '../../global/reducers/auth';
-import { LOGIN_SUCCESS } from '../LoginPage/constants';
 
 function* tokenAssign({ payload: { token } }) {
   yield put(setToken(token));
@@ -9,5 +8,5 @@ function* tokenAssign({ payload: { token } }) {
 }
 
 export default function* rootSaga() {
-  yield all([takeEvery(LOGIN_SUCCESS, tokenAssign)]);
+  yield all([takeEvery(action => /SUCCESS$/.test(action.type), tokenAssign)]);
 }
