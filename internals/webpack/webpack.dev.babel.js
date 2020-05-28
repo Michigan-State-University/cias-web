bcb373419e501dd5c4d033d2e2931abaa8dc749f
Michal Grzelak <grzelakm@outlook.com>
2020-05-28 18:22:18 +0200

    Move Dotenv to dev webpack

--- internals/webpack/webpack.dev.babel.js
+++ internals/webpack/webpack.dev.babel.js
@@ -6,6 +6,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = require('./webpack.base.babel')({
   mode: 'development',
@@ -40,6 +41,7 @@ module.exports = require('./webpack.base.babel')({
       exclude: /a\.js|node_modules/, // exclude node_modules
       failOnError: false, // show a warning when there is a circular dependency
     }),
+    new Dotenv(),
   ],
 
   // Emit a source map for easier debugging
