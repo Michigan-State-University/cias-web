ba8acc4bbd250130590e8633770910984d5fe8c1
Michal Grzelak <grzelakm@outlook.com>
2020-05-29 12:18:13 +0200

    [CIAS-34] Remove unnecessary comments messages from single and multi questions

--- app/containers/Interventions/components/QuestionData/MultiQuestion/messages.js
+++ app/containers/Interventions/components/QuestionData/MultiQuestion/messages.js
@@ -1,12 +1,6 @@
-/*
- * CreateInterventionPage Messages
- *
- * This contains all the text for the CreateInterventionPage container.
- */
-
 import { defineMessages } from 'react-intl';
 
-export const scope = 'app.containers.Single';
+export const scope = 'app.containers.Multi';
 
 export default defineMessages({
   placeholder: {
