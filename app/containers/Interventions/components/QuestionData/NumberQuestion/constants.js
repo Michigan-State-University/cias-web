ce43acf6b9cb35f1786a2c6160fd29b949fc9425
Michal Grzelak <grzelakm@outlook.com>
2020-06-01 10:17:38 +0200

    Remove unused constants

--- app/containers/Interventions/components/QuestionData/NumberQuestion/constants.js
+++ app/containers/Interventions/components/QuestionData/NumberQuestion/constants.js
@@ -1,3 +1,1 @@
-export const ADD = 'app/EditInterventionPage/Number/ADD';
 export const UPDATE = 'app/EditInterventionPage/Number/UPDATE';
-export const REMOVE = 'app/EditInterventionPage/Number/REMOVE';
