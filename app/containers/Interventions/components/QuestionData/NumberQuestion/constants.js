2db098dc9f18dd0aacadd0ad007f770a633f44d8
Michal Grzelak <grzelakm@outlook.com>
2020-06-01 10:09:31 +0200

    Add NumberQuestion component

---
+++ app/containers/Interventions/components/QuestionData/NumberQuestion/constants.js
@@ -0,0 +1,3 @@
+export const ADD = 'app/EditInterventionPage/Number/ADD';
+export const UPDATE = 'app/EditInterventionPage/Number/UPDATE';
+export const REMOVE = 'app/EditInterventionPage/Number/REMOVE';
---
+++ app/containers/Interventions/components/QuestionData/NumberQuestion/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { injectIntl } from 'react-intl';
+
+import Row from 'components/Row';
+import Question from 'models/Intervention/Question';
+import ApprovableInput from 'components/Input/ApprovableInput';
+import Box from 'components/Box';
+
+import { colors } from 'theme/colors';
+import messages from './messages';
+import { UPDATE } from './constants';
+
+import { makeSelectSelectedQuestion } from '../../../containers/EditInterventionPage/selectors';
+import { updateQuestionData } from '../../../containers/EditInterventionPage/actions';
+
+const NumberQuestion = ({
+  selectedQuestion,
+  updateAnswer,
+  intl: { formatMessage },
+}) => {
+  const { variable, payload } = selectedQuestion.body.data[0];
+  return (
+    <Box bg={colors.linkWater} width="100%" px={21} py={14} mt={20}>
+      <Row>
+        <ApprovableInput
+          rows="5"
+          placeholder={formatMessage(messages.placeholder)}
+          value={payload}
+          onCheck={newTitle => updateAnswer({ variable, payload: newTitle })}
+        />
+      </Row>
+    </Box>
+  );
+};
+
+NumberQuestion.propTypes = {
+  selectedQuestion: PropTypes.shape(Question).isRequired,
+  intl: PropTypes.object.isRequired,
+  updateAnswer: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  selectedQuestion: makeSelectSelectedQuestion(),
+});
+
+const mapDispatchToProps = dispatch => ({
+  updateAnswer: value =>
+    dispatch(updateQuestionData({ type: UPDATE, data: { value } })),
+});
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default injectIntl(compose(withConnect)(NumberQuestion));
