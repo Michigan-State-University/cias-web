cd3b82ae4db83f628d9bd15dcff4a2349ed354d5
Michal Grzelak <grzelakm@outlook.com>
2020-06-29 12:46:00 +0200

    Add variable to VisualAnalogueScale

--- app/containers/Interventions/components/QuestionData/VisualAnalogueScaleQuestion/index.js
+++ app/containers/Interventions/components/QuestionData/VisualAnalogueScaleQuestion/index.js
@@ -10,11 +10,13 @@ import Column from 'components/Column';
 import Question from 'models/Intervention/Question';
 import { StyledInput } from 'components/Input/StyledInput';
 import Box from 'components/Box';
+import { BadgeInput } from 'components/Input/BadgeInput';
 
 import { colors } from 'theme/colors';
 import AppSlider from 'components/AppSlider';
+import globalMessages from 'global/i18n/globalMessages';
 import messages from './messages';
-import { UPDATE_DATA } from './constants';
+import { UPDATE_DATA, UPDATE_VARIABLE } from './constants';
 
 import { makeSelectSelectedQuestion } from '../../../containers/EditInterventionPage/selectors';
 import { updateQuestionData } from '../../../containers/EditInterventionPage/actions';
@@ -22,46 +24,65 @@ import { updateQuestionData } from '../../../containers/EditInterventionPage/act
 const VisualAnalogueScaleQuestion = ({
   selectedQuestion,
   updateLabel,
+  updateVariable,
   intl: { formatMessage },
 }) => {
-  const startValue = selectedQuestion.body.data[0].payload.start_value;
-  const endValue = selectedQuestion.body.data[0].payload.end_value;
+  const {
+    variable,
+    payload: { start_value: startValue, end_value: endValue },
+  } = selectedQuestion.body.data[0];
+
   return (
-    <Box width="100%" px={21} py={14}>
-      <Column>
-        <Row>
-          <Box width="100%">
-            <AppSlider />
-          </Box>
-        </Row>
+    <Column>
+      <BadgeInput
+        px={0}
+        py={12}
+        mb={10}
+        textAlign="center"
+        keyboard="tel"
+        placeholder={formatMessage(
+          globalMessages.variables.variableNamePlaceholder,
+        )}
+        value={variable.name}
+        color={colors.jungleGreen}
+        onBlur={val => updateVariable(val)}
+      />
+      <Box width="100%" px={21} py={14}>
+        <Column>
+          <Row>
+            <Box width="100%">
+              <AppSlider />
+            </Box>
+          </Row>
 
-        <Row justify="between" filled>
-          <Box hoverColor={colors.linkWater} padding={5}>
-            <StyledInput
-              width={120}
-              px={0}
-              py={9}
-              textAlign="center"
-              placeholder={formatMessage(messages.startValue)}
-              value={startValue}
-              onBlur={value => updateLabel(value, 'start_value')}
-            />
-          </Box>
+          <Row justify="between" filled>
+            <Box hoverColor={colors.linkWater} padding={5}>
+              <StyledInput
+                width={120}
+                px={0}
+                py={9}
+                textAlign="center"
+                placeholder={formatMessage(messages.startValue)}
+                value={startValue}
+                onBlur={value => updateLabel(value, 'start_value')}
+              />
+            </Box>
 
-          <Box hoverColor={colors.linkWater} padding={5}>
-            <StyledInput
-              width={120}
-              px={0}
-              py={9}
-              textAlign="center"
-              placeholder={formatMessage(messages.endValue)}
-              value={endValue}
-              onBlur={value => updateLabel(value, 'end_value')}
-            />
-          </Box>
-        </Row>
-      </Column>
-    </Box>
+            <Box hoverColor={colors.linkWater} padding={5}>
+              <StyledInput
+                width={120}
+                px={0}
+                py={9}
+                textAlign="center"
+                placeholder={formatMessage(messages.endValue)}
+                value={endValue}
+                onBlur={value => updateLabel(value, 'end_value')}
+              />
+            </Box>
+          </Row>
+        </Column>
+      </Box>
+    </Column>
   );
 };
 
@@ -69,6 +90,7 @@ VisualAnalogueScaleQuestion.propTypes = {
   selectedQuestion: PropTypes.shape(Question).isRequired,
   intl: PropTypes.object.isRequired,
   updateLabel: PropTypes.func.isRequired,
+  updateVariable: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = createStructuredSelector({
@@ -78,6 +100,8 @@ const mapStateToProps = createStructuredSelector({
 const mapDispatchToProps = {
   updateLabel: (value, label) =>
     updateQuestionData({ type: UPDATE_DATA, data: { value, label } }),
+  updateVariable: name =>
+    updateQuestionData({ type: UPDATE_VARIABLE, data: { name } }),
 };
 
 const withConnect = connect(
