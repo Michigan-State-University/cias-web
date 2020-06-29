e3523d193661eba274789a6ccb603210748ca4f1
Michal Grzelak <grzelakm@outlook.com>
2020-06-29 12:33:53 +0200

    Update Number Question with variable name

--- app/containers/Interventions/components/QuestionData/NumberQuestion/index.js
+++ app/containers/Interventions/components/QuestionData/NumberQuestion/index.js
@@ -6,14 +6,17 @@ import { createStructuredSelector } from 'reselect';
 import { injectIntl } from 'react-intl';
 
 import Row from 'components/Row';
+import Column from 'components/Column';
 import Question from 'models/Intervention/Question';
 import ApprovableInput from 'components/Input/ApprovableInput';
 import Box from 'components/Box';
+import { BadgeInput } from 'components/Input/BadgeInput';
 
 import { colors } from 'theme/colors';
 import { numericValidator } from 'utils/validators';
+import globalMessages from 'global/i18n/globalMessages';
 import messages from './messages';
-import { UPDATE_DATA } from './constants';
+import { UPDATE_DATA, UPDATE_VARIABLE } from './constants';
 
 import { makeSelectSelectedQuestion } from '../../../containers/EditInterventionPage/selectors';
 import { updateQuestionData } from '../../../containers/EditInterventionPage/actions';
@@ -21,22 +24,38 @@ import { updateQuestionData } from '../../../containers/EditInterventionPage/act
 const NumberQuestion = ({
   selectedQuestion,
   updateAnswer,
+  updateVariable,
   intl: { formatMessage },
 }) => {
   const { variable, payload } = selectedQuestion.body.data[0];
   return (
-    <Box bg={colors.linkWater} width="100%" px={21} py={14}>
-      <Row>
-        <ApprovableInput
-          type="singleline"
-          keyboard="tel"
-          placeholder={formatMessage(messages.placeholder)}
-          value={payload}
-          validator={numericValidator}
-          onCheck={newTitle => updateAnswer({ variable, payload: newTitle })}
-        />
-      </Row>
-    </Box>
+    <Column mt={10}>
+      <BadgeInput
+        px={0}
+        py={12}
+        mb={10}
+        textAlign="center"
+        keyboard="tel"
+        placeholder={formatMessage(
+          globalMessages.variables.variableNamePlaceholder,
+        )}
+        value={variable.name}
+        color={colors.jungleGreen}
+        onBlur={val => updateVariable(val)}
+      />
+      <Box bg={colors.linkWater} width="100%" px={21} py={14}>
+        <Row>
+          <ApprovableInput
+            type="singleline"
+            keyboard="tel"
+            placeholder={formatMessage(messages.placeholder)}
+            value={payload}
+            validator={numericValidator}
+            onCheck={newTitle => updateAnswer({ variable, payload: newTitle })}
+          />
+        </Row>
+      </Box>
+    </Column>
   );
 };
 
@@ -44,6 +63,7 @@ NumberQuestion.propTypes = {
   selectedQuestion: PropTypes.shape(Question).isRequired,
   intl: PropTypes.object.isRequired,
   updateAnswer: PropTypes.func.isRequired,
+  updateVariable: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = createStructuredSelector({
@@ -53,6 +73,8 @@ const mapStateToProps = createStructuredSelector({
 const mapDispatchToProps = dispatch => ({
   updateAnswer: value =>
     dispatch(updateQuestionData({ type: UPDATE_DATA, data: { value } })),
+  updateVariable: name =>
+    updateQuestionData({ type: UPDATE_VARIABLE, data: { name } }),
 });
 
 const withConnect = connect(
