20cb632af3edb41830ec4c437bcef77daea21b59
Michal Grzelak <grzelakm@outlook.com>
2020-07-03 14:08:17 +0200

    Remove styled.js from TargetQuestionChooser and VariableChooser

--- app/containers/Interventions/components/QuestionSettings/TargetQuestionChooser/styled.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import StyledCircle from 'components/Circle/StyledCircle';
-
-const DotCircle = props => <StyledCircle {...props} size="10px" />;
-
-export { DotCircle };
