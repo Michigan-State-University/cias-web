0ab36cfa8a55fbc392ff820de47065a747faf720
Michal Grzelak <grzelakm@outlook.com>
2020-07-21 10:20:26 +0200

    Add systemvars to dotenv

--- internals/webpack/webpack.dev.babel.js
+++ internals/webpack/webpack.dev.babel.js
@@ -41,7 +41,7 @@ module.exports = require('./webpack.base.babel')({
       exclude: /a\.js|node_modules/, // exclude node_modules
       failOnError: false, // show a warning when there is a circular dependency
     }),
-    new Dotenv(),
+    new Dotenv({ systemvars: true }),
   ],
 
   // Emit a source map for easier debugging
