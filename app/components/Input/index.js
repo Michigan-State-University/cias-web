ee916008a655c78a2cbb233076e94e9699c7ebb3
PiotrSokoliński <piotr.sokolinski@htdevelopers.com>
2020-07-23 12:35:35 +0200

    Fix border input on error

--- app/components/Input/index.js
+++ app/components/Input/index.js
@@ -1,28 +1,36 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { themeColors, borders, paddings } from 'theme';
+import { themeColors, borders, paddings, colors } from 'theme';
 import { margin, layout, padding, text } from '../BaseComponentStyles';
+
+const getBorderColor = (hasError, currentColor) => {
+  if (hasError) return colors.flamingo;
+  return currentColor;
+};
 
 const Input = styled.input.attrs(props => ({ type: props.keyboard }))`
   padding: ${paddings.small};
-  border: ${borders.borderWidth} ${borders.borderStyle}
-    ${props => (props.hasError ? 'red' : themeColors.highlight)};
+  border-style: ${borders.borderStyle};
+  border-width: ${borders.borderWidth};
+  border-color: ${({ hasError }) =>
+    getBorderColor(hasError, themeColors.highlight)};
   border-radius: ${borders.borderRadius};
   &:focus {
     box-shadow: none;
     outline: none;
-    border: ${borders.borderWidth} ${borders.borderStyle} ${themeColors.primary};
+    border-color: ${({ hasError }) =>
+      getBorderColor(hasError, themeColors.primary)};
   }
+  ${({ transparent }) =>
+    transparent && {
+      backgroundColor: 'transparent',
+      borderColor: 'transparent',
+    }};
+  ${props => (props.textAlign ? { textAlign: props.textAlign } : {})};
   ${margin};
   ${layout};
   ${padding};
   ${text};
-  ${({ transparent }) =>
-    transparent && {
-      backgroundColor: 'transparent',
-      border: `${borders.borderWidth} ${borders.borderStyle} transparent`,
-    }};
-  ${props => (props.textAlign ? { textAlign: props.textAlign } : {})};
 `;
 
 Input.propTypes = {
