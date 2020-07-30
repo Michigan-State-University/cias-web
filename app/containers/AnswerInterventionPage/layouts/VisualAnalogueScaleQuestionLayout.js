7cea1dc8346c70580a1588a409aa7de604acc9e0
Jakub Zygmunt <67907594+Jakub-Zygmunt@users.noreply.github.com>
2020-07-30 14:21:03 +0200

    Fix scale values (#110)

--- app/containers/AnswerInterventionPage/layouts/VisualAnalogueScaleQuestionLayout.js
+++ app/containers/AnswerInterventionPage/layouts/VisualAnalogueScaleQuestionLayout.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import toNumber from 'lodash/toNumber';
-import isNaN from 'lodash/isNaN';
 
 import Column from 'components/Column';
 import Row from 'components/Row';
@@ -14,35 +12,29 @@ const VisualAnalogueScaleQuestionLayout = ({
   startValue,
   endValue,
   answerValue,
-}) => {
-  const parsedStart = toNumber(startValue);
-  const parsedEnd = toNumber(endValue);
-  const start = !isNaN(parsedStart) ? parsedStart : null;
-  const end = !isNaN(parsedEnd) ? parsedEnd : null;
-  return (
-    <Column mt={10} mb={10}>
-      <Box width="100%">
-        <Row>
-          <Box width="100%" px={21} py={14}>
-            <AppSlider
-              showValue
-              step={1}
-              onChange={onChange}
-              value={answerValue}
-              onAfterChange={onAfterChange}
-              min={start}
-              max={end}
-            />
-          </Box>
-        </Row>
-        <Row justify="between" filled padding="5px">
-          <Box>{startValue}</Box>
-          <Box>{endValue}</Box>
-        </Row>
-      </Box>
-    </Column>
-  );
-};
+}) => (
+  <Column mt={10} mb={10}>
+    <Box width="100%">
+      <Row>
+        <Box width="100%" px={21} py={14}>
+          <AppSlider
+            showValue
+            step={1}
+            onChange={onChange}
+            value={answerValue}
+            onAfterChange={onAfterChange}
+            min={0}
+            max={100}
+          />
+        </Box>
+      </Row>
+      <Row justify="between" filled padding="5px">
+        <Box>{startValue}</Box>
+        <Box>{endValue}</Box>
+      </Row>
+    </Box>
+  </Column>
+);
 
 VisualAnalogueScaleQuestionLayout.propTypes = {
   onChange: PropTypes.func,
