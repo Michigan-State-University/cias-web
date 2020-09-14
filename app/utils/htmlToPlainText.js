858f1e2dbe54adc3de13096a90b5e2abbeb2aa82
Jakub Zygmunt <67907594+Jakub-Zygmunt@users.noreply.github.com>
2020-09-14 09:33:41 +0200

    [CIAS-457] - Better handling of some special chars in screen titles  (#216)
    * Fix htmlToPlainText function
    
    * Change htmlToPlainText function
    
    * Use lodash in htmlToPlainText function

--- app/utils/htmlToPlainText.js
+++ app/utils/htmlToPlainText.js
@@ -1,1 +1,6 @@
-export const htmlToPlainText = html => html.replace(/<[^>]*>?/gm, '');
+import unescape from 'lodash/unescape';
+
+export const htmlToPlainText = html => {
+  const text = html.replace(/<[^>]*>?/gm, '');
+  return unescape(text);
+};
