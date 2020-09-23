bd1b0515a1977c0cd4f7e360a9812548d20c8307
Michał Śnieć <michal.sniec@htdevelopers.com>
2020-09-23 08:15:39 +0200

    Code review changes

--- app/global/reducers/user/actions.js
+++ app/global/reducers/user/actions.js
@@ -24,18 +24,21 @@ export const fetchUserRequest = id => actionBuilder(FETCH_USER, { id });
 export const fetchUserSuccess = user => actionBuilder(FETCH_USER_SUCCESS, user);
 export const fetchUserFailure = error =>
   actionBuilder(FETCH_USER_FAILURE, error);
+
 export const editOtherUserRequest = userData =>
   actionBuilder(EDIT_OTHER_USER_REQUEST, userData);
 export const editOtherUserSuccess = user =>
   actionBuilder(EDIT_OTHER_USER_SUCCESS, { user });
 export const editOtherUserError = error =>
   actionBuilder(EDIT_OTHER_USER_ERROR, error);
+
 export const addOtherUserAvatarRequest = userData =>
   actionBuilder(ADD_OTHER_USER_AVATAR_REQUEST, userData);
 export const addOtherUserAvatarSuccess = user =>
   actionBuilder(ADD_OTHER_USER_AVATAR_SUCCESS, { user });
 export const addOtherUserAvatarError = error =>
   actionBuilder(ADD_OTHER_USER_AVATAR_ERROR, error);
+
 export const deleteOtherUserAvatarRequest = userId =>
   actionBuilder(DELETE_OTHER_USER_AVATAR_REQUEST, { userId });
 export const deleteOtherUserAvatarSuccess = user =>
