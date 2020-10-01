4ff6768cc108691b3388e0e2c280d9395aaac1bf
michal-grzelak <grzelakm@outlook.com>
2020-10-01 11:33:16 +0200

    [CIAS-423] E-mail confirmation toast (#279)
    * Display account confirmation string
    
    * Bump up lodash and query-string versions

--- app/containers/LoginPage/constants.js
+++ app/containers/LoginPage/constants.js
@@ -7,3 +7,8 @@
 export const LOGIN_REQUEST = 'app/LoginPage/LOGIN_REQUEST';
 export const LOGIN_SUCCESS = 'app/LoginPage/LOGIN_SUCCESS';
 export const LOGIN_ERROR = 'app/LoginPage/LOGIN_ERROR';
+
+export const ACCOUNT_CONFIRMATION_SUCCESS =
+  'app/LoginPage/ACCOUNT_CONFIRMATION_SUCCESS';
+export const ACCOUNT_CONFIRMATION_ERROR =
+  'app/LoginPage/ACCOUNT_CONFIRMATION_ERROR';
