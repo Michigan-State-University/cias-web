934dd34d331e6ae3a2794deddbf60eb38c00e699
Michał Śnieć <michal.sniec@htdevelopers.com>
2020-10-23 10:04:46 +0200

    Fix variables lodaing

--- app/global/reducers/questionGroups/index.js
+++ app/global/reducers/questionGroups/index.js
@@ -14,6 +14,7 @@ export {
   makeSelectQuestionGroups,
   makeSelectDefaultGroupId,
   makeSelectQuestionGroupsLoader,
+  makeSelectGetQuestionGroupLoader,
 } from './selectors';
 export {
   copyQuestionsSaga,
