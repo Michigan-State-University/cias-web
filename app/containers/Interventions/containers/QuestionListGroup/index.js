cdaee3bb60d3ff2ae0b948cfb46b649e6dc15cb2
Michal Grzelak <grzelakm@outlook.com>
2020-10-29 12:33:50 +0100

    Add possibility to render Group without dnd

--- app/containers/Interventions/containers/QuestionListGroup/index.js
+++ app/containers/Interventions/containers/QuestionListGroup/index.js
@@ -19,7 +19,7 @@ import reorderIcon from 'assets/svg/reorder-hand.svg';
 
 import { reorderScope } from 'models/Intervention/ReorderScope';
 import QuestionListItem from '../../components/QuestionListItem';
-import { DraggableContainer } from './styled';
+import { Spacer, DraggableContainer } from './styled';
 import messages from './messages';
 
 const QuestionListGroup = ({
@@ -37,6 +37,7 @@ const QuestionListGroup = ({
   problemStatus,
   index: groupIndex,
   formatMessage,
+  notDnd,
 }) => {
   const { title, id } = questionGroup;
   const [openCollapsable, setOpenCollapsable] = useState(true);
@@ -46,94 +47,114 @@ const QuestionListGroup = ({
     setOpenCollapsable(true);
   }, [questions.length]);
 
-  if (questions.length === 0) return <></>;
-  return (
-    <Draggable key={`group-${id}`} draggableId={id} index={groupIndex}>
-      {providedGroupDraggable => (
-        <Row
-          width="100%"
-          display="block"
-          ref={providedGroupDraggable.innerRef}
-          {...providedGroupDraggable.draggableProps}
-        >
-          <Collapse
-            disabled
-            isOpened={openCollapsable}
-            onToggle={toggleCollapsable}
-            height="auto"
-            px={0}
-            bgOpacity={0}
-            onHideImg={arrowDown}
-            onShowImg={arrowUp}
-            imgWithBackground
-            label={
-              <Row align="center" justify="between" width="100%" mr={10}>
-                <Box display="flex">
-                  {manage && (
-                    <Checkbox
-                      mr={2}
-                      onClick={e => {
-                        e.stopPropagation();
-                        toggleGroup(questions);
-                      }}
-                      checked={checkSelectedGroup(questions)}
-                    />
-                  )}
-                  <StyledInput
-                    px={12}
-                    py={6}
-                    value={title}
-                    fontSize={18}
-                    fontWeight="bold"
-                    placeholder={formatMessage(messages.groupPlaceholder)}
-                    width="100%"
-                    maxWidth="initial"
-                    onBlur={val => changeGroupName(val, interventionId, id)}
-                  />
-                </Box>
-                <Img
-                  src={reorderIcon}
-                  {...providedGroupDraggable.dragHandleProps}
-                />
-              </Row>
-            }
-          >
-            <Droppable
-              key={`group-${questionGroup.id}`}
-              droppableId={id}
-              type={reorderScope.questions}
-            >
-              {provided => (
-                <DraggableContainer
-                  style={{ width: '100%' }}
-                  ref={provided.innerRef}
-                  {...provided.droppableProps}
-                >
-                  {questions.map((question, index) => (
-                    <Row key={question.id} width="100%">
-                      <QuestionListItem
-                        selectSlide={selectSlide}
-                        checked={selectedSlides.includes(question.id)}
-                        manage={manage}
-                        index={index}
-                        selectedQuestionIndex={selectedQuestion}
-                        questions={questions}
-                        question={question}
-                        interventionId={interventionId}
-                        disabled={editingPossible}
-                        problemStatus={problemStatus}
-                      />
-                    </Row>
-                  ))}
-                  {provided.placeholder}
-                </DraggableContainer>
-              )}
-            </Droppable>
-          </Collapse>
+  const renderQuestions = providedGroupDroppable => (
+    <DraggableContainer
+      style={{ width: '100%' }}
+      {...!notDnd && {
+        ref: providedGroupDroppable.innerRef,
+        ...providedGroupDroppable.droppableProps,
+      }}
+    >
+      {questions.map((question, index) => (
+        <Row key={question.id} width="100%">
+          <QuestionListItem
+            selectSlide={selectSlide}
+            checked={selectedSlides.includes(question.id)}
+            manage={manage}
+            index={index}
+            selectedQuestionIndex={selectedQuestion}
+            questions={questions}
+            question={question}
+            interventionId={interventionId}
+            disabled={editingPossible}
+            problemStatus={problemStatus}
+            notDnd={notDnd}
+          />
         </Row>
-      )}
+      ))}
+      {notDnd && providedGroupDroppable.placeholder}
+    </DraggableContainer>
+  );
+
+  const renderQuestionsWithDnd = () => (
+    <Droppable
+      key={`group-${questionGroup.id}`}
+      droppableId={id}
+      type={reorderScope.questions}
+    >
+      {provided => renderQuestions(provided)}
+    </Droppable>
+  );
+
+  const renderGroup = providedGroupDraggable => (
+    <Row
+      width="100%"
+      display="block"
+      {...(notDnd
+        ? { key: `group-${id}` }
+        : {
+            ref: providedGroupDraggable.innerRef,
+            ...providedGroupDraggable.draggableProps,
+          })}
+    >
+      <Collapse
+        disabled
+        isOpened={openCollapsable}
+        onToggle={toggleCollapsable}
+        height="auto"
+        px={0}
+        bgOpacity={0}
+        onHideImg={arrowDown}
+        onShowImg={arrowUp}
+        imgWithBackground
+        label={
+          <Row align="center" justify="between" width="100%" mr={10}>
+            <Box display="flex">
+              {manage && (
+                <Checkbox
+                  mr={2}
+                  onClick={e => {
+                    e.stopPropagation();
+                    toggleGroup(questions);
+                  }}
+                  checked={checkSelectedGroup(questions)}
+                />
+              )}
+              <StyledInput
+                px={12}
+                py={6}
+                value={title}
+                fontSize={18}
+                fontWeight="bold"
+                placeholder={formatMessage(messages.groupPlaceholder)}
+                width="100%"
+                maxWidth="initial"
+                onBlur={val => changeGroupName(val, interventionId, id)}
+              />
+            </Box>
+            {!notDnd && (
+              <Img
+                src={reorderIcon}
+                {...providedGroupDraggable.dragHandleProps}
+              />
+            )}
+          </Row>
+        }
+      >
+        {notDnd ? renderQuestions() : renderQuestionsWithDnd()}
+      </Collapse>
+      <Spacer />
+    </Row>
+  );
+
+  const renderGroupWithDnd = () => (
+    <Draggable key={`group-${id}`} draggableId={id} index={groupIndex}>
+      {providedGroupDraggable => renderGroup(providedGroupDraggable)}
     </Draggable>
   );
+
+  if (questions.length === 0) return <></>;
+  return notDnd ? renderGroup() : renderGroupWithDnd();
 };
 
 QuestionListGroup.propTypes = {
@@ -152,6 +173,7 @@ QuestionListGroup.propTypes = {
   problemStatus: PropTypes.string,
   isDuringQuestionReorder: PropTypes.bool,
   index: PropTypes.number,
+  notDnd: PropTypes.bool,
 };
 
 const mapStateToProps = (_, props) =>
