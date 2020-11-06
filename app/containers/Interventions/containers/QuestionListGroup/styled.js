a4c0166dd268e4b69fd84925cf4b4eae33828b55
Michal Grzelak <grzelakm@outlook.com>
2020-11-06 11:22:34 +0100

    Update snapshot

--- app/containers/Interventions/containers/QuestionListGroup/styled.js
+++ app/containers/Interventions/containers/QuestionListGroup/styled.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
+import { colors, paddings } from 'theme';
 
 export const DraggableContainer = styled.div`
   width: 100%;
+`;
+
+export const Spacer = styled.div`
+  height: 1px;
+  width: calc(100% + ${paddings.regular} * 2);
+  margin-left: -${paddings.regular};
+  background-color: ${colors.linkWater};
 `;
