9ea1aadc650bd86ccb5cb20d807b442f2a57273f
Jakub Zygmunt <67907594+Jakub-Zygmunt@users.noreply.github.com>
2020-11-09 13:21:10 +0100

    [CIAS-673] - Add Cypress to project (#362)
    * Init Cypress
    
    * Add prettier cypress rules
    
    * Remove useless plugin value

---
+++ cypress/fixtures/fixtures.js
@@ -0,0 +1,7 @@
+/*
+Fixtures are used as external pieces of static data that can be used by
+your tests.Fixture files are located in cypress/fixtures by default, but
+can be configured to another directory.You would typically use them
+with the cy.fixture() command and most often when you’re stubbing
+Network Requests
+ */
