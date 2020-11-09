4699105b35e823fb1c86e616e977e058ddee4117
Michał Śnieć <michal.sniec@htdevelopers.com>
2020-11-09 15:20:34 +0100

    Add login integration tests

--- cypress/integration/signIn/signIn.spec.js
+++ cypress/integration/signIn/signIn.spec.js
@@ -1,5 +1,39 @@
-describe('Test to delete', () => {
-  it('Test to delete', () => {
-    expect(true).to.equal(true);
+/* eslint no-unused-expressions: 0 */ // --> OFF
+
+const {
+  ADMIN_EMAIL,
+  ADMIN_PASSWORD,
+  API_URL,
+} = require('../../support/envVariables');
+
+const loginAddress = Cypress.env(API_URL);
+
+describe('Login test', () => {
+  beforeEach(() => {
+    cy.visit(loginAddress);
+  });
+
+  it('Should display correct errors', () => {
+    cy.get('button').click();
+    expect(cy.contains('Email is required')).to.exist;
+    expect(cy.contains('Password is required')).to.exist;
+  });
+
+  it('Should display error on invalid email', () => {
+    cy.get('input[name=email]')
+      .type('some-name')
+      .blur();
+    expect(cy.contains('Provide valid email')).to.exist;
+  });
+
+  it('Should login correctly', () => {
+    cy.get('input[name=email]')
+      .type(Cypress.env(ADMIN_EMAIL))
+      .blur();
+    cy.get('input[name=password]')
+      .type(Cypress.env(ADMIN_PASSWORD))
+      .blur();
+    cy.get('button').click();
+    expect(cy.contains('My Dashboard')).to.exist;
   });
 });
