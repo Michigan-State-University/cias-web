a2bdf8784c99287255cbbddc93ac0d45025c9d24
Jakub Zygmunt <67907594+Jakub-Zygmunt@users.noreply.github.com>
2020-11-13 08:48:55 +0100

    [CIAS-688] - Change start intervention copy (#374)
    * Change copy
    
    * Change copy

--- app/containers/AnswerInterventionPage/index.js
+++ app/containers/AnswerInterventionPage/index.js
@@ -282,6 +282,14 @@ export function AnswerInterventionPage({
     onStartIntervention();
   };
 
+  const startButtonText = () => {
+    if (previewPossible || isNullOrUndefined(previewPossible))
+      return isPreview
+        ? formatMessage(messages.startPreview)
+        : formatMessage(messages.startSession);
+    return formatMessage(messages.previewDisabled);
+  };
+
   const renderPage = () => <Fragment>{renderQuestion()}</Fragment>;
 
   if (questionLoading) return <Loader />;
@@ -313,11 +321,7 @@ export function AnswerInterventionPage({
             <StyledButton
               disabled={!previewPossible}
               onClick={startInterventionAsync}
-              title={
-                previewPossible || isNullOrUndefined(previewPossible)
-                  ? formatMessage(messages.startIntervention)
-                  : formatMessage(messages.previewDisabled)
-              }
+              title={startButtonText()}
               isDesktop={isDesktop}
             />
           )}
