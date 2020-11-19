33a973542bdbc450c866c549d08b489881154ae5
Jakub Zygmunt <67907594+Jakub-Zygmunt@users.noreply.github.com>
2020-11-19 13:49:27 +0100

    [CIAS-703] - Fix chips input (#381)
    * Fix chips input
    
    * Change validator to yup
    
    * Fix test

--- app/utils/tests/validators/emailValidator.test.js
+++ app/utils/tests/validators/emailValidator.test.js
@@ -7,7 +7,6 @@ import { emailValidator } from 'utils/validators';
 describe('emailValidator regex', () => {
   it('should return true when checked against valid emails', () => {
     const targets = [
-      '', // empty
       'test@com.pl',
       'partcipant@cias-api.herokuapp.com',
       'partcipant@gmail.com',
@@ -18,6 +17,7 @@ describe('emailValidator regex', () => {
 
   it('should return false when checked against non-valid emails', () => {
     const targets = [
+      '', // empty
       'test', // no domain
       'test@com', // invalid domain
       'test@comp.pl.',
