30ec5725a9f5838c98682724394bda042dc6795f
Michal Grzelak <grzelakm@outlook.com>
2020-12-14 15:01:51 +0100

    [CIAS30-561] Create confirm email, reset password and csv download templates (#415)
    * Create confirm email, reset password and csv download templates
    
    * Add invite to session template

--- mjml/components/AppLayout.js
+++ mjml/components/AppLayout.js
@@ -1,48 +1,83 @@
-import { registerDependencies } from 'mjml-validator';
-import { BodyComponent } from 'mjml-core';
+import { registerDependencies } from 'mjml-validator'
+import { BodyComponent } from 'mjml-core'
+import { ciasLogo } from '../assets/rawImages'
 
 registerDependencies({
   // Tell the validator which tags are allowed as our component's children
   'app-layout': ['*'],
   'mj-body': ['app-layout'],
-});
+})
 
 export default class AppLayout extends BodyComponent {
   constructor(initialDatas = {}) {
-    super(initialDatas);
-    this.cssId = Math.floor(Math.random() * 9) + 1;
+    super(initialDatas)
+    this.cssId = Math.floor(Math.random() * 9) + 1
   }
 
   // Tells the validator which attributes are allowed for app-layout
-  static allowedAttributes = {};
+  static allowedAttributes = {
+    header: 'string',
+    mainText: 'string',
+    buttonText: 'string',
+    buttonUrl: 'string',
+    footNote: 'string',
+    buttonColor: 'color',
+    buttonTextColor: 'color',
+  }
 
   // Exactly what the name suggests. Fallback value for this.getAttribute('attribute-name').
-  static defaultAttributes = {};
+  static defaultAttributes = {
+    header: 'Header text',
+    mainText: 'Main <span style="color:red;">text</span>!',
+    buttonText: 'Link',
+    buttonUrl: '#',
+    footNote: 'Footnote',
+    buttonColor: '#107969',
+    buttonTextColor: '#FFF',
+  }
 
   render() {
+    const header = this.getAttribute('header')
+    const mainText = this.getAttribute('mainText')
+    const buttonText = this.getAttribute('buttonText')
+    const buttonUrl = this.getAttribute('buttonUrl')
+    const footNote = this.getAttribute('footNote')
+    const buttonColor = this.getAttribute('buttonColor')
+    const buttonTextColor = this.getAttribute('buttonTextColor')
     /*
       Components are supposed to return html. If we want to return mjml so as to
       use existing components, we need to process it manually using this.renderMJML()
     */
     return this.renderMJML(`
-      <mj-wrapper background-color="#808080" full-width="full-width">
-        <mj-section background-color="#fff">
+      <mj-wrapper padding="0" background-color="#FFF">
+        <mj-section full-width="full-width">
           <mj-column>
-            <mj-text align="center" font-size="20px" color="blue">
-              App Header
-            </mj-text>
+            <mj-image width="100%" src="${ciasLogo}" />
           </mj-column>
         </mj-section>
         <mj-section>
-          ${this.renderChildren(this.props.children, {
-            /* The rawXML option prevents processing on children : we already call this.renderMJML on the whole block so we don't want the children to be processed twice */
-            rawXML: true,
-
-            /* The renderer option allows to use a specific rendering function, or wrap each child if needed. Below is the default, see mj-column code for an example of this. */
-            renderer: component => component.render,
-          })}
+          <mj-column>
+           <mj-text align="center" font-weight="700" font-size="30px">${header}</mj-text>
+          </mj-column>
         </mj-section>
-      </mj-wrapper>
-		`);
+        <mj-section>
+          <mj-column>
+           <mj-text align="center" font-weight="700" font-size="20px">${mainText}</mj-text>
+          </mj-column>
+        </mj-section>
+        <mj-section>
+          <mj-column>
+            <mj-button border="1px solid ${buttonColor}" border-radius="100px" background-color="${buttonColor}" color="${buttonTextColor}" font-size="16px" font-weight="700" href="${buttonUrl}">
+              ${buttonText}
+            </mj-button>
+          </mj-column>
+        </mj-section>
+        ${footNote &&
+          `<mj-section>
+        <mj-column>
+         <mj-text align="center" font-size="15px">${footNote}</mj-text>
+        </mj-column>
+      </mj-section>`}
+      </mj-wrapper>`)
   }
 }
