6aa9605e2574d1b6d7014edd487d43a48e00944b
Michal Grzelak <grzelakm@outlook.com>
2021-04-28 09:09:51 +0200

    [CIAS30-1222] Right Side Panel (#620)
    * Sidepanel WIP
    
    * Position SidePanel
    
    * Implement SidePanel
    
    * Fix test comment component name

---
+++ app/components/SidePanel/index.js
@@ -0,0 +1,66 @@
+/**
+ *
+ * SidePanel
+ *
+ */
+
+import React, { memo, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { StyledSidePanel, HandleWrapper, SidePanelWrapper } from './styled';
+
+const SidePanel = ({ Handle, onOpen, onClose, isOpen, style, children }) => {
+  const [isVisible, setIsVisible] = useState(isOpen);
+  const open = () => {
+    if (!isVisible) {
+      setIsVisible(true);
+
+      if (onOpen) onOpen();
+    }
+  };
+  const close = () => {
+    if (isVisible) {
+      setIsVisible(false);
+
+      if (onClose) onClose();
+    }
+  };
+  const toggle = () => {
+    if (isVisible) close();
+    else open();
+  };
+
+  useEffect(() => {
+    if (isOpen) open();
+    else close();
+  }, [isOpen]);
+
+  const { width } = style;
+
+  return (
+    <>
+      <SidePanelWrapper $isVisible={isVisible} width={width}>
+        {Handle && <HandleWrapper onClick={toggle}>{Handle}</HandleWrapper>}
+        <StyledSidePanel width={width}>{children}</StyledSidePanel>
+      </SidePanelWrapper>
+    </>
+  );
+};
+
+SidePanel.propTypes = {
+  Handle: PropTypes.node,
+  style: PropTypes.shape({
+    width: PropTypes.number,
+  }),
+  onOpen: PropTypes.func,
+  onClose: PropTypes.func,
+  isOpen: PropTypes.bool,
+  children: PropTypes.node,
+};
+
+SidePanel.defaultProps = {
+  isOpen: false,
+  style: { width: 300 },
+};
+
+export default memo(SidePanel);
---
+++ app/components/SidePanel/styled.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+import { boxShadows, colors, elements } from 'theme';
+
+import { margin } from 'components/BaseComponentStyles';
+
+export const StyledSidePanel = styled.div`
+  width: ${({ width }) => `${width}px`};
+  height: 100%;
+  background-color: ${colors.white};
+  box-shadow: ${boxShadows.black};
+`;
+
+export const SidePanelWrapper = styled.div`
+  position: fixed;
+  display: inline-flex;
+  z-index: 1;
+  height: calc(100vh - ${elements.navbarHeight}px);
+  top: ${elements.navbarHeight}px;
+  right: ${({ $isVisible, width }) => ($isVisible ? 0 : `${-width}px`)};
+  transition: right 0.4s ease;
+`;
+
+export const HandleWrapper = styled.div`
+  width: max-content;
+  margin-right: 16px;
+  margin-top: 16px;
+  z-index: 10;
+  ${margin};
+`;
