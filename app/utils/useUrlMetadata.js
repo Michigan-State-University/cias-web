b3ee8f14d4062766f01acb6d289fb13e2cdd9e09
Michal Grzelak <grzelakm@outlook.com>
2021-06-22 10:29:40 +0200

    [CIAS30-1383] URL Metadata (#674)
    * Add initial link preview implementation with app api
    
    * Update tests
    
    * Change url preview image
    
    * Update url metadata fetching with new api
    
    * Bump version

---
+++ app/utils/useUrlMetadata.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import objectToCamelCase from 'utils/objectToCamelCase';
+
+export const useUrlMetadata = url => {
+  const [state, setState] = useState({
+    metadata: null,
+    error: null,
+    isFetching: false,
+  });
+
+  const fetchUrlMetadata = async () =>
+    axios.get('/v1/show_website_metadata', { params: { url } });
+
+  useEffect(() => {
+    if (url) {
+      setState({ ...state, isFetching: true });
+
+      fetchUrlMetadata()
+        .then(({ data }) => {
+          const metadata = objectToCamelCase(data);
+
+          setState({ metadata, error: null, isFetching: false });
+        })
+        .catch(error => {
+          setState({ metadata: null, error, isFetching: false });
+        });
+    }
+  }, [url]);
+
+  return state;
+};
