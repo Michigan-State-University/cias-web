6e6e1ca4db384e7b207122e91d56c075c819e3ab
msniec <michal.sniec@gmail.com>
2021-09-21 11:30:38 +0200

    Remove unneccesery lib

--- app/utils/getErrorFlag.js
+++ app/utils/getErrorFlag.js
@@ -1,9 +1,7 @@
-import XRegExp from 'xregexp';
-
 const getErrorFlag = (error) => {
   const message = error.response?.data?.message;
   if (!message) return null;
-  const matchedRegexp = XRegExp.exec(message, /ERROR_FLAG:([a-zA-Z]*)/);
+  const matchedRegexp = message.match(/ERROR_FLAG:([a-zA-Z]*)/);
   if (!matchedRegexp || matchedRegexp.length < 2) return null;
   return matchedRegexp[1];
 };
