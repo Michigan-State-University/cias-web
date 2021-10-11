d586ddf5edda75203604c0a56f1633ebf9387ba9
bartosz-kepka-htd <bartosz.kepka@htdevelopers.com>
2021-10-11 11:50:41 +0200

    [CIAS30-1767] Make question settings type strict

--- app/models/Question/Question.ts
+++ app/models/Question/Question.ts
@@ -5,7 +5,25 @@ import { Formula } from 'models/Formula';
 import { Narrator } from 'models/Narrator';
 
 import { QuestionTypes } from './QuestionTypes';
-import { QuestionSettings } from './QuestionSettings';
+import {
+  CurrencyQuestionSettings,
+  DateQuestionSettings,
+  ExternalLinkQuestionSettings,
+  FeedbackQuestionSettings,
+  FinishQuestionSettings,
+  FreeResponseQuestionSettings,
+  GridQuestionSettings,
+  InformationQuestionSettings,
+  MultipleQuestionSettings,
+  NameQuestionSettings,
+  NumberQuestionSettings,
+  ParticipantReportQuestionSettings,
+  PhoneQuestionSettings,
+  QuestionSettings,
+  SingleQuestionSettings,
+  SliderQuestionSettings,
+  ThirdPartyQuestionSettings,
+} from './QuestionSettings';
 import {
   SingleQuestionData,
   MultipleQuestionData,
@@ -34,13 +52,14 @@ import { QuestionOriginalText } from './QuestionOriginalText';
 export type QuestionFormulaTargetType = QuestionTypes | SessionTargetType;
 
 export interface GenericQuestion<
-  VType extends QuestionTypes = QuestionTypes,
-  TBody extends QuestionBodyType = QuestionBodyType,
+  VType extends QuestionTypes,
+  TBody extends QuestionBodyType,
+  TSettings extends QuestionSettings,
 > {
   id: string;
   type: VType;
   questionGroupId: string;
-  settings: QuestionSettings;
+  settings: TSettings;
   position: number;
   title: string;
   subtitle: string;
@@ -55,99 +74,115 @@ export interface GenericQuestion<
 
 export type SingleQuestion = GenericQuestion<
   QuestionTypes.SINGLE,
-  QuestionBodyWithVariable<SingleQuestionData>
+  QuestionBodyWithVariable<SingleQuestionData>,
+  SingleQuestionSettings
 >;
 export type SingleQuestionDTO = CamelToSnake<SingleQuestion>;
 
 export type MultipleQuestion = GenericQuestion<
   QuestionTypes.MULTIPLE,
-  QuestionBodyWithoutVariable<MultipleQuestionData>
+  QuestionBodyWithoutVariable<MultipleQuestionData>,
+  MultipleQuestionSettings
 >;
 export type MultipleQuestionDTO = CamelToSnake<MultipleQuestion>;
 
 export type FreeResponseQuestion = GenericQuestion<
   QuestionTypes.FREE_RESPONSE,
-  QuestionBodyWithVariable<FreeResponseQuestionData>
+  QuestionBodyWithVariable<FreeResponseQuestionData>,
+  FreeResponseQuestionSettings
 >;
 export type FreeResponseQuestionDTO = CamelToSnake<FreeResponseQuestion>;
 
 export type ThirdPartyReportQuestion = GenericQuestion<
   QuestionTypes.THIRD_PARTY,
-  QuestionBodyWithoutVariable<ThirdPartyReportQuestionData>
+  QuestionBodyWithoutVariable<ThirdPartyReportQuestionData>,
+  ThirdPartyQuestionSettings
 >;
 export type ThirdPartyReportQuestionDTO =
   CamelToSnake<ThirdPartyReportQuestion>;
 
 export type NameQuestion = GenericQuestion<
   QuestionTypes.NAME,
-  QuestionBodyWithVariable<NameQuestionData>
+  QuestionBodyWithVariable<NameQuestionData>,
+  NameQuestionSettings
 >;
 export type NameQuestionDTO = CamelToSnake<NameQuestion>;
 
 export type NumberQuestion = GenericQuestion<
   QuestionTypes.NUMBER,
-  QuestionBodyWithVariable<NumberQuestionData>
+  QuestionBodyWithVariable<NumberQuestionData>,
+  NumberQuestionSettings
 >;
 export type NumberQuestionDTO = CamelToSnake<NumberQuestion>;
 
 export type GridQuestion = GenericQuestion<
   QuestionTypes.GRID,
-  QuestionBodyWithoutVariable<GridQuestionData>
+  QuestionBodyWithoutVariable<GridQuestionData>,
+  GridQuestionSettings
 >;
 export type GridQuestionDTO = CamelToSnake<GridQuestion>;
 
 export type SliderQuestion = GenericQuestion<
   QuestionTypes.SLIDER,
-  QuestionBodyWithVariable<SliderQuestionData>
+  QuestionBodyWithVariable<SliderQuestionData>,
+  SliderQuestionSettings
 >;
 export type SliderQuestionDTO = CamelToSnake<SliderQuestion>;
 
 export type InformationQuestion = GenericQuestion<
   QuestionTypes.INFORMATION,
-  QuestionBodyWithoutVariable<InformationOnlyQuestionData>
+  QuestionBodyWithoutVariable<InformationOnlyQuestionData>,
+  InformationQuestionSettings
 >;
 export type InformationQuestionDTO = CamelToSnake<InformationQuestion>;
 
 export type ExternalLinkQuestion = GenericQuestion<
   QuestionTypes.EXTERNAL_LINK,
-  QuestionBodyWithVariable<ExternalLinkQuestionData>
+  QuestionBodyWithVariable<ExternalLinkQuestionData>,
+  ExternalLinkQuestionSettings
 >;
 export type ExternalLinkQuestionDTO = CamelToSnake<ExternalLinkQuestion>;
 
 export type FeedbackQuestion = GenericQuestion<
   QuestionTypes.FEEDBACK,
-  QuestionBodyWithoutVariable<FeedbackQuestionData>
+  QuestionBodyWithoutVariable<FeedbackQuestionData>,
+  FeedbackQuestionSettings
 >;
 export type FeedbackQuestionDTO = CamelToSnake<FeedbackQuestion>;
 
 export type FinishQuestion = GenericQuestion<
   QuestionTypes.FINISH,
-  QuestionBodyWithoutVariable<FinishQuestionData>
+  QuestionBodyWithoutVariable<FinishQuestionData>,
+  FinishQuestionSettings
 >;
 export type FinishQuestionDTO = CamelToSnake<FinishQuestion>;
 
 export type PhoneQuestion = GenericQuestion<
   QuestionTypes.PHONE,
-  QuestionBodyWithVariable<PhoneQuestionData>
+  QuestionBodyWithVariable<PhoneQuestionData>,
+  PhoneQuestionSettings
 >;
 export type PhoneQuestionDTO = CamelToSnake<PhoneQuestion>;
 
 export type DateQuestion = GenericQuestion<
   QuestionTypes.DATE,
-  QuestionBodyWithVariable<DateQuestionData>
+  QuestionBodyWithVariable<DateQuestionData>,
+  DateQuestionSettings
 >;
 export type DateQuestionDTO = CamelToSnake<DateQuestion>;
 
 export type ParticipantReportQuestion = GenericQuestion<
   QuestionTypes.PARTICIPANT_REPORT,
-  QuestionBodyWithVariable<ParticipantReportQuestionData>
+  QuestionBodyWithVariable<ParticipantReportQuestionData>,
+  ParticipantReportQuestionSettings
 >;
 export type ParticipantReportQuestionDTO =
   CamelToSnake<ParticipantReportQuestion>;
 
 export type CurrencyQuestion = GenericQuestion<
   QuestionTypes.CURRENCY,
-  QuestionBodyWithVariable<CurrencyQuestionData>
+  QuestionBodyWithVariable<CurrencyQuestionData>,
+  CurrencyQuestionSettings
 >;
 export type CurrencyQuestionDTO = CamelToSnake<CurrencyQuestion>;
 
--- app/models/Question/QuestionSettings.ts
+++ app/models/Question/QuestionSettings.ts
@@ -1,11 +1,95 @@
-export interface QuestionSettings {
+export interface QuestionBaseSettings {
   image: boolean;
   title: boolean;
   video: boolean;
-  required?: boolean;
   subtitle: boolean;
-  textLimit?: number;
   narratorSkippable: boolean;
-  proceedButton?: boolean;
-  showNumber?: boolean;
 }
+
+export interface ProceedButtonSetting {
+  proceedButton: boolean;
+}
+
+export interface RequiredSetting {
+  required: boolean;
+}
+
+export interface SingleQuestionSettings
+  extends QuestionBaseSettings,
+    RequiredSetting,
+    ProceedButtonSetting {}
+
+export interface MultipleQuestionSettings
+  extends QuestionBaseSettings,
+    RequiredSetting {}
+
+export interface FreeResponseQuestionSettings
+  extends QuestionBaseSettings,
+    RequiredSetting {
+  textLimit: number;
+}
+
+export interface ThirdPartyQuestionSettings
+  extends QuestionBaseSettings,
+    RequiredSetting {}
+
+export interface NameQuestionSettings
+  extends QuestionBaseSettings,
+    RequiredSetting {}
+
+export interface NumberQuestionSettings
+  extends QuestionBaseSettings,
+    RequiredSetting {}
+
+export interface GridQuestionSettings
+  extends QuestionBaseSettings,
+    RequiredSetting,
+    ProceedButtonSetting {}
+
+export interface SliderQuestionSettings
+  extends QuestionBaseSettings,
+    RequiredSetting {
+  showNumber: boolean;
+}
+
+export interface InformationQuestionSettings extends QuestionBaseSettings {}
+
+export interface ExternalLinkQuestionSettings extends QuestionBaseSettings {}
+
+export interface FeedbackQuestionSettings extends QuestionBaseSettings {}
+
+export interface FinishQuestionSettings extends QuestionBaseSettings {}
+
+export interface PhoneQuestionSettings
+  extends QuestionBaseSettings,
+    RequiredSetting {}
+
+export interface DateQuestionSettings
+  extends QuestionBaseSettings,
+    RequiredSetting {}
+
+export interface ParticipantReportQuestionSettings
+  extends QuestionBaseSettings,
+    RequiredSetting {}
+
+export interface CurrencyQuestionSettings
+  extends QuestionBaseSettings,
+    RequiredSetting {}
+
+export type QuestionSettings =
+  | SingleQuestionSettings
+  | MultipleQuestionSettings
+  | FreeResponseQuestionSettings
+  | ThirdPartyQuestionSettings
+  | NameQuestionSettings
+  | NumberQuestionSettings
+  | GridQuestionSettings
+  | SliderQuestionSettings
+  | InformationQuestionSettings
+  | ExternalLinkQuestionSettings
+  | FeedbackQuestionSettings
+  | FinishQuestionSettings
+  | PhoneQuestionSettings
+  | DateQuestionSettings
+  | ParticipantReportQuestionSettings
+  | CurrencyQuestionSettings;
