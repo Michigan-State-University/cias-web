6aee3a1ad838c4c7dc25f33c50a2c281b9751c18
bartosz-kepka-htd <bartosz.kepka@htdevelopers.com>
2022-01-18 19:53:02 +0100

    [CIAS30-2014] Style TLFB settings container

--- app/containers/Sessions/components/TlfbDetails/TlfbDetails.tsx
+++ app/containers/Sessions/components/TlfbDetails/TlfbDetails.tsx
@@ -1,14 +1,53 @@
 import React from 'react';
+import { useIntl } from 'react-intl';
+import { useSelector } from 'react-redux';
 
 import { QuestionGroup } from 'models/QuestionGroup';
+import { canEdit } from 'models/Status/statusPermissions';
 
-import { TlfbDetailsContainer } from './styled';
+import { makeSelectInterventionStatus } from 'global/reducers/intervention';
+
+import StyledInput from 'components/Input/StyledInput';
+import { selectInputText } from 'components/Input/utils';
+
+import {
+  TlfbContainer,
+  TlfbHeaderContainer,
+  TlfbSettingsContainer,
+} from './styled';
+import messages from '../QuestionDetails/messages';
 
 export type TlfbDetailsProps = {
   questionGroup: QuestionGroup;
   changeGroupName: (newName: string) => void;
 };
 
-export const TlfbDetails = ({ questionGroup }: TlfbDetailsProps) => (
-  <TlfbDetailsContainer>{JSON.stringify(questionGroup)}</TlfbDetailsContainer>
-);
+export const TlfbDetails = ({
+  questionGroup,
+  changeGroupName,
+}: TlfbDetailsProps) => {
+  const { formatMessage } = useIntl();
+
+  const interventionStatus = useSelector(makeSelectInterventionStatus());
+  const editingPossible = canEdit(interventionStatus);
+
+  return (
+    <TlfbContainer>
+      <TlfbHeaderContainer>
+        <StyledInput
+          // @ts-ignore
+          px={12}
+          value={questionGroup.title}
+          fontSize={18}
+          fontWeight="bold"
+          placeholder={formatMessage(messages.groupPlaceholder)}
+          maxWidth="initial"
+          onFocus={selectInputText}
+          onBlur={(val) => changeGroupName(val)}
+          disabled={!editingPossible}
+        />
+      </TlfbHeaderContainer>
+      <TlfbSettingsContainer></TlfbSettingsContainer>
+    </TlfbContainer>
+  );
+};
--- app/containers/Sessions/components/TlfbDetails/styled.tsx
+++ app/containers/Sessions/components/TlfbDetails/styled.tsx
@@ -5,11 +5,23 @@ import { colors, elements } from 'theme';
 
 const containerSpacing = 32;
 
-// @ts-ignore
-export const TlfbDetailsContainer = styled.div`
-  border: 1px dashed ${colors.mystic};
+export const TlfbContainer = styled.div`
   margin: ${containerSpacing}px;
-  padding: ${containerSpacing}px;
   width: calc(100% - ${elements.screenSettingsWidth + 2 * containerSpacing}px);
+  display: flex;
+  flex-direction: column;
+`;
+
+export const TlfbHeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+export const TlfbSettingsContainer = styled.div`
+  border: 1px dashed ${colors.mystic};
+  padding: ${containerSpacing}px;
   background: ${colors.white};
+  flex: 1;
 `;
