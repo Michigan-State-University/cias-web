1414727e27edeae3c5a594dc063d2729bdd04f61
msniec <michal.sniec@gmail.com>
2022-02-07 14:48:20 +0100

    Code review

--- app/global/reducers/tlfb/sagas/fetchCalendarData.ts
+++ app/global/reducers/tlfb/sagas/fetchCalendarData.ts
@@ -13,9 +13,14 @@ import {
 function* fetchCalendarData({
   payload: { userSessionId, questionGroupId },
 }: ReturnType<typeof fetchCalendarDataRequest>) {
-  const url = `/v1/calendar_data?user_session_id=${userSessionId}&tlfb_group_id=${questionGroupId}`;
+  const url = `/v1/calendar_data`;
+
+  const params = {
+    user_session_id: userSessionId,
+    tlfb_group_id: questionGroupId,
+  };
   try {
-    const { data } = yield call(axios.get, url);
+    const { data } = yield call(axios.get, url, { params });
     const days = jsonApiToArray(data, 'day');
     const mappedDays = days.reduce(
       (obj: Record<string, DayData>, { date, events, substances }: any) => ({
