e344f8f8713047b9f7d3dfcb7e67a41d9a6983cf
msniec <michal.sniec@gmail.com>
2022-03-07 09:08:37 +0100

    WIP infinite scroll

--- app/components/CopyModal/Components/ChooserComponent.js
+++ app/components/CopyModal/Components/ChooserComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, memo } from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl, IntlShape } from 'react-intl';
 
@@ -17,7 +17,77 @@ import arrowRightActive from 'assets/svg/arrow-right-active.svg';
 import arrowBack from 'assets/svg/arrow-back.svg';
 import { hexToRgb, themeColors, colors } from 'theme';
 import isNullOrUndefined from 'utils/isNullOrUndefined';
+import { VirtualGrid } from 'components/VirtualList';
 import messages from './messages';
+
+const XD = ({ data, index }) => {
+  const { items } = data;
+  const element = items?.[index - 1];
+  console.log(element);
+  const { id, name, sessions_size: sessionsSize } = element || {};
+  const renderSizeLabel = (value, valueMsg, noValueMsg) => {
+    if (!isNullOrUndefined(value)) {
+      if (value === 0) return <Text fontSize={12}>{noValueMsg}</Text>;
+      return (
+        <Text fontSize={12}>
+          {valueMsg}: {value}
+        </Text>
+      );
+    }
+  };
+  // const isDisabled = count =>
+  //   disableCopy && !isNullOrUndefined(count) && count === 0;
+  return (
+    <Column key={`${id}-select-target-question-group-${index}`}>
+      <Row
+        width="100%"
+        // disabled={isDisabled(sessionsSize)}
+        clickable
+        align="center"
+        justify="start"
+        hoverColor={`rgba(${hexToRgb(themeColors.secondary)}, 0.1)`}
+        // onClick={() =>
+        //   disableCopy
+        //     ? changeViewAction({ id, name })
+        //     : selectAction({ id, name })
+        // }
+        px={25}
+        py={15}
+      >
+        {/* <Img src={listIcon} mr={10} /> */}
+        <Row mr={10} width="100%" justify="between" align="center">
+          <Column>
+            <Box maxWidth={170}>
+              <EllipsisText text={name} fontSize={16} />
+            </Box>
+            {/* {renderSizeLabel(
+              sessionsSize,
+              formatMessage(messages.sessions),
+              formatMessage(messages.noSessions),
+            )} */}
+          </Column>
+          <Row justify="center" align="center" mr={10}>
+            {/* {!disableCopy ? (
+              <Radio checked={id === selectedItem?.id} />
+            ) : (
+              <Icon src={arrowRightActive} />
+            )} */}
+          </Row>
+        </Row>
+      </Row>
+      <Row mx={25}>
+        {index !== items.length - 1 && <Divider color={colors.linkWater} />}
+      </Row>
+    </Column>
+  );
+};
+
+XD.propTypes = {
+  data: PropTypes.object,
+  index: PropTypes.number,
+};
+
+const Item = memo(XD);
 
 const ChooserComponent = ({
   intl: { formatMessage },
@@ -34,22 +104,10 @@ const ChooserComponent = ({
   disableCopy,
   listIcon,
 }) => {
-  const renderSizeLabel = (value, valueMsg, noValueMsg) => {
-    if (!isNullOrUndefined(value)) {
-      if (value === 0) return <Text fontSize={12}>{noValueMsg}</Text>;
-      return (
-        <Text fontSize={12}>
-          {valueMsg}: {value}
-        </Text>
-      );
-    }
-  };
-
-  const isDisabled = count =>
-    disableCopy && !isNullOrUndefined(count) && count === 0;
-
+  const infiniteLoaderRef = useRef();
+  console.log(items);
   return (
-    <Column data-testid={`${elementId}-select-target-session`} height={280}>
+    <Column data-testid={`${elementId}-select-target-session`} height="100%">
       <Column mx={25} mb={15}>
         {backText && (
           <Row align="center" clickable onClick={() => backAction()}>
@@ -80,7 +138,23 @@ const ChooserComponent = ({
               <Loader type="inline" hidden={false} />
             </Box>
           )}
-          {!loading &&
+          <Box filled>
+            <VirtualGrid
+              ref={infiniteLoaderRef}
+              columnCount={1}
+              rowCount={items.length}
+              rowHeight={65}
+              items={items}
+              // infiniteLoader={{
+              //   loadMoreItems: () => {},
+              //   isLoading: false,
+              //   itemCount: items.length,
+              // }}
+            >
+              {Item}
+            </VirtualGrid>
+          </Box>
+          {/* {!loading &&
             !!items?.length &&
             items.map(({ id, name, sessions_size: sessionsSize }, index) => (
               <Column
@@ -128,7 +202,7 @@ const ChooserComponent = ({
                   )}
                 </Row>
               </Column>
-            ))}
+            ))} */}
           {!loading && !items?.length && (
             <Column>
               <Row px={25} py={15}>
