3e819739b425a53589dc56458732949ae49008aa
Bartosz Kępka <86963976+bartosz-kepka-htd@users.noreply.github.com>
2022-06-07 10:19:40 +0200

    [CIAS30-2448] Encode connection query params (#1168)

--- app/components/ActionCable/SocketProvider.tsx
+++ app/components/ActionCable/SocketProvider.tsx
@@ -7,8 +7,9 @@ import React, {
 import { createCable, fetchTokenFromHTML } from '@anycable/web';
 import { Cable } from '@anycable/core';
 
-import LocalStorageService from 'utils/localStorageService';
 import { User } from 'models/User';
+import LocalStorageService from 'utils/localStorageService';
+import { URIEncodeObject } from 'utils/uriUtils';
 
 type Props = PropsWithChildren<{
   user: User;
@@ -19,12 +20,14 @@ export const SocketContext = createContext<Nullable<Cable>>(null);
 export const SocketProvider = ({ children, user }: Props) => {
   const headers = LocalStorageService.getHeaders();
 
+  const URIEncodedHeaders = URIEncodeObject(headers);
+
   const cable = useRef<Nullable<Cable>>(null);
 
   useEffect(() => {
     if (user) {
       cable.current = createCable(
-        `${process.env.WEBSOCKET_URL}?access_token=${headers['Access-Token']}&uid=${headers.Uid}&client=${headers.Client}`,
+        `${process.env.WEBSOCKET_URL}?access_token=${URIEncodedHeaders['Access-Token']}&uid=${URIEncodedHeaders.Uid}&client=${URIEncodedHeaders.Client}`,
         { tokenRefresher: fetchTokenFromHTML() },
       );
     } else {
