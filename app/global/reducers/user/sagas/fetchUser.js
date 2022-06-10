f081bc47c914a1eed50c06edac0a3e9a1fa82a4d
Bartosz Kępka <86963976+bartosz-kepka-htd@users.noreply.github.com>
2022-06-10 15:45:33 +0200

    [CIAS30-2457] Map quickExitEnabled in the current user response (#1173)
    * [CIAS30-2457] Map quickExitEnabled in the current user response
    
    * Change node version
    
    * [CIAS30-2457] CR changes and fix displaying avatar
    
    * [CIAS30-2457] Add action for fetch self details success

--- app/global/reducers/user/sagas/fetchUser.js
+++ app/global/reducers/user/sagas/fetchUser.js
@@ -1,8 +1,7 @@
 import { takeLatest, put, call } from 'redux-saga/effects';
 import axios from 'axios';
 
-import { pickUserAttributes } from 'utils/mapResponseObjects';
-import { jsonApiToObject } from 'utils/jsonApiMapper';
+import { pickUserAttributes, mapCurrentUser } from 'utils/mapResponseObjects';
 import { FETCH_USER_REQUEST } from '../constants';
 
 import { fetchUserFailure, fetchUserSuccess } from '../actions';
@@ -10,8 +9,10 @@ import { fetchUserFailure, fetchUserSuccess } from '../actions';
 export function* fetchUser({ payload: { id } }) {
   const requestUrl = `/v1/users/${id}`;
   try {
-    const { data } = yield call(axios.get, requestUrl);
-    const user = jsonApiToObject(data, 'user');
+    const {
+      data: { data },
+    } = yield call(axios.get, requestUrl);
+    const user = mapCurrentUser(data);
     const pickedUser = pickUserAttributes(user);
     yield put(fetchUserSuccess(pickedUser));
   } catch (error) {
