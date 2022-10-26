ff7a7a1159b44ac6b8a29e290edf471bd8e720e4
Klara Jędrzejewska <52409023+mkjedrzejewska@users.noreply.github.com>
2022-10-26 13:04:45 +0200

    [CIAS30-2924] Update mail template according to design (#1423)

--- mjml/components/AppLayout.js
+++ mjml/components/AppLayout.js
@@ -53,12 +53,14 @@ export default class AppLayout extends BodyComponent {
     const doNotReply = this.getAttribute('doNotReply')
     const buttonColor = this.getAttribute('buttonColor')
     const buttonTextColor = this.getAttribute('buttonTextColor')
+    const description = this.getAttribute('description')
+    const otherInformation = this.getAttribute('otherInformation')
 
     const textWithBreak = (text) => textBuilder(text, [textSpaceDecorator])
 
     const textBuilder = (text, decorators) => (decorators ? flow(...decorators)(text) : text)
 
-    const textSpaceDecorator = (text) => `${text}<br/><br/> `
+    const textSpaceDecorator = (text) => `${text}<br/>`
 
     const createFootNote = () => {
       const textArray = []
@@ -85,34 +87,44 @@ export default class AppLayout extends BodyComponent {
         <mj-section full-width="full-width">
           <mj-column>
             <! -- LOGO START -->
-            <mj-image width="100%" src="${LOGO_URL}" />
+            <mj-image width="66px" max-width="100%" src="${LOGO_URL}" />
             <! -- LOGO END -->
           </mj-column>
         </mj-section>
-        <mj-section>
+        <mj-section padding="16px 0 24px 0">
           <mj-column>
-
-          <mj-text align="center" font-weight="700" font-size="30px">
-            <! -- HEADER START -->
-            ${header}
-            <! -- HEADER END -->
-          </mj-text>
-
+            <mj-text align="center" font-weight="700" font-size="22px" line-height="29px">
+              <! -- HEADER START -->
+              ${header}
+              <! -- HEADER END -->
+            </mj-text>
           </mj-column>
-          </mj-section>
-          <mj-section>
+        </mj-section>
+        <mj-section padding="0px 0px 0px 0px">
           <mj-column>
-          <mj-text align="center" font-weight="700" font-size="20px">
-            <! -- MAIN-TEXT START -->
-            ${mainText}
-            <! -- MAIN-TEXT END -->
-          </mj-text>
+            <mj-text align="center" font-weight="700" font-size="16px" line-height="26px">
+              <! -- MAIN-TEXT START -->
+              ${mainText}
+              <! -- MAIN-TEXT END -->
+            </mj-text>
           </mj-column>
         </mj-section>
         ${
+          description &&
+          `
+            <mj-section padding="4px 0px 0px 0px">
+              <mj-column>
+                <mj-text align="center" font-size="14px" line-height="24px">
+                  ${description}
+                </mj-text>
+              </mj-column>
+            </mj-section>
+          `
+        }
+        ${
           buttonUrl &&
           buttonText &&
-          `<mj-section>
+          `<mj-section padding="32px 0px 0px 0px">
           <mj-column>
             <mj-button border="1px solid ${buttonColor}" border-radius="100px" background-color="${buttonColor}" color="${buttonTextColor}" font-size="16px" font-weight="700" href="${buttonUrl}">
             <! -- BUTTON START -->
@@ -123,16 +135,29 @@ export default class AppLayout extends BodyComponent {
         </mj-section>`
         }
         ${
+          otherInformation &&
+          `
+          <mj-section>
+            <mj-column>
+              <mj-divider border-width="1px" border-color="#E9F1FB" padding="12px 0 22px 0"/>
+              <mj-text align="center" font-size="14px" line-height="24px" padding-left="32px" padding-right="32px">
+                ${otherInformation}
+              </mj-text>
+            </mj-column>
+          </mj-section>
+        `
+        }
+        ${
           hasFootNote &&
           `<mj-section>
-        <mj-column>
-         <mj-text align="center" font-size="15px">
-         <! -- FOOTNOTE START -->
-         ${footNote}
-         <! -- FOOTNOTE END -->
-         </mj-text>
-        </mj-column>
-      </mj-section>`
+            <mj-column>
+              <mj-text align="center" font-size="12px" color="#6D7485" line-height="20px">
+              <! -- FOOTNOTE START -->
+              ${footNote}
+              <! -- FOOTNOTE END -->
+              </mj-text>
+            </mj-column>
+          </mj-section>`
         }
       </mj-wrapper>`)
   }
