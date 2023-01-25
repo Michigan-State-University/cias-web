4a578d08ae59256a9d382cba7983a356f3cd7f05
Bartosz Kępka <86963976+bartosz-kepka-htd@users.noreply.github.com>
2023-01-25 15:31:05 +0100

    [CIAS30-3137] Update copy (#1527)

--- app/components/HenryFordBrachingInfoModal/messages.ts
+++ app/components/HenryFordBrachingInfoModal/messages.ts
@@ -9,7 +9,7 @@ export default defineMessages({
   },
   content: {
     id: `${scope}.content`,
-    defaultMessage: `It looks like you want to duplicate {type, select, session {a session in an intervention} other {an intervention}} which includes Henry Ford's integration. <bold>Access to the Henry Ford's integration for the copy will need to be granted by a CIAS admin again</bold>. Also, Henry Ford Initial Screens will be removed from the copy together with all of their branching connections.`,
+    defaultMessage: `It looks like you want to duplicate {type, select, session {a session in the intervention} other {the intervention}} which includes Henry Ford's integration. <bold>Access to the Henry Ford's integration for the copy will need to be granted by a CIAS admin again</bold>. Also, Henry Ford Initial Screens will be removed from the copy together with all of their branching connections.`,
   },
   okay: {
     id: `${scope}.okay`,
