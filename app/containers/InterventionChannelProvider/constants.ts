7f31241a9cdd114a0b787c4043946f8524de4205
Bartosz Kępka <86963976+bartosz-kepka-htd@users.noreply.github.com>
2023-06-13 09:51:36 +0200

    [CIAS30-3441] Fix typo in intervention channel routes (#8)

--- app/containers/InterventionChannelProvider/constants.ts
+++ app/containers/InterventionChannelProvider/constants.ts
@@ -1,12 +1,12 @@
-const BASE_INTERVENTION_ROUTE = '/interventions/:interventionId';
-const SPECIFIC_INTERVENTION_ROUTE = `${BASE_INTERVENTION_ROUTE}/sessions/:sessionId/`;
+const INTERVENTION_ROUTE = '/interventions/:interventionId';
+const SESSION_ROUTE = `${INTERVENTION_ROUTE}/sessions/:sessionId`;
 
 export const INTERVENTION_CHANNEL_ROUTES = [
-  BASE_INTERVENTION_ROUTE,
-  `${SPECIFIC_INTERVENTION_ROUTE}/edit`,
-  `${SPECIFIC_INTERVENTION_ROUTE}/settings`,
-  `${SPECIFIC_INTERVENTION_ROUTE}/report-templates`,
-  `${SPECIFIC_INTERVENTION_ROUTE}/sms-messaging`,
-  `${SPECIFIC_INTERVENTION_ROUTE}/generated-reports`,
-  `${SPECIFIC_INTERVENTION_ROUTE}/map`,
+  INTERVENTION_ROUTE,
+  `${SESSION_ROUTE}/edit`,
+  `${SESSION_ROUTE}/settings`,
+  `${SESSION_ROUTE}/report-templates`,
+  `${SESSION_ROUTE}/sms-messaging`,
+  `${SESSION_ROUTE}/generated-reports`,
+  `${SESSION_ROUTE}/map`,
 ];
