3639223eb7e49067ff396dd3cfbbec236c1588c8
bartosz-kepka-htd <bartosz.kepka@htdevelopers.com>
2023-07-31 16:11:39 +0200

    [CIAS30-3495] Change limits to be based on 1000 multiplier

--- app/global/constants/file.ts
+++ app/global/constants/file.ts
@@ -1,1 +1,2 @@
 export const MAX_FILE_SIZE = 5242880;
+export const BYTES_IN_MB = 5000000;
--- app/global/constants/mms.ts
+++ app/global/constants/mms.ts
@@ -1,4 +1,4 @@
-import { MAX_FILE_SIZE } from './file';
+import { BYTES_IN_MB } from './file';
 
 export const MMS_LARGE_IMAGE_FILE_FORMATS = [
   'image/jpeg',
@@ -49,14 +49,14 @@ export const MMS_ACCEPTED_FILE_FORMATS = [
 
 // See https://support.twilio.com/hc/en-us/articles/360018832773-Twilio-Programmable-SMS-Supported-File-Types-and-Size-Limits-for-MMS-Media-Messages#:~:text=Number%20of%20attachments%20supported&text=An%20MMS%20message%20body%20can,your%20message%20request%20will%20fail.
 // "An MMS message body can be up to 1600 characters long, which equals 4.8KB..."
-// We are rounding up to 10 kB for validation messages readability in MMSes sent outside the US
-export const MMS_MAX_TEXT_SIZE = 10240;
+// We are rounding up to 10KB for validation messages readability in MMSes sent outside the US
+export const MMS_MAX_TEXT_SIZE = 10000;
 
-// 4.99 kB
-export const MMS_MAX_FILE_SIZE = MAX_FILE_SIZE - MMS_MAX_TEXT_SIZE;
+// 5MB - 10KB = 4.99MB
+export const MMS_MAX_FILE_SIZE = 5 * BYTES_IN_MB - MMS_MAX_TEXT_SIZE;
 
-// 600 kB
-export const MMS_MAX_NON_LARGE_IMAGE_FILE_FORMAT_SIZE = 614400;
+// 600KB
+export const MMS_MAX_NON_LARGE_IMAGE_FILE_FORMAT_SIZE = 600000;
 
 export const MMS_MAX_LARGE_IMAGE_FILE_SIZE_MAP = new Map<string, number>(
   MMS_LARGE_IMAGE_FILE_FORMATS.map((format) => [format, MMS_MAX_FILE_SIZE]),
