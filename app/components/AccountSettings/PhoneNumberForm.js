9e0762015793f6eae4711041a5e2da62a957d34c
bartosz-kepka-htd <bartosz.kepka@htdevelopers.com>
2023-08-03 17:11:30 +0200

    Cherrypick phone number form changes from HF branch

--- app/components/AccountSettings/PhoneNumberForm.js
+++ app/components/AccountSettings/PhoneNumberForm.js
@@ -1,28 +1,25 @@
-import React, { useEffect, useRef, useMemo, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import isEqual from 'lodash/isEqual';
-import union from 'lodash/union';
-import { Formik, yupToFormErrors, validateYupSchema } from 'formik';
+import { Formik } from 'formik';
 import parsePhoneNumber, {
   getCountryCallingCode,
   formatIncompletePhoneNumber,
 } from 'libphonenumber-js';
-import { FlagIcon } from 'react-flag-kit';
 
-import FormikNumberInput from 'components/FormikNumberInput';
-import FormikSelect from 'components/FormikSelect';
 import ErrorAlert from 'components/ErrorAlert';
 import Row from 'components/Row';
-import Text from 'components/Text';
 import Column from 'components/Column';
+import {
+  DEFAULT_COUNTRY_CODE,
+  FormikPhoneNumberInput,
+  phoneNumberSchema,
+} from 'components/FormikPhoneNumberInput';
 
-import getCountriesCodes from 'utils/getCountriesCodes';
 import isNullOrUndefined from 'utils/isNullOrUndefined';
 
 import { ConfirmButton } from './styled';
 import messages from './messages';
-import { popularPrefixes } from './constants';
-import phoneNumberSchema from './schemas/phoneNumberSchema';
 import PhoneNumberCodeModal from './PhoneNumberCodeModal';
 
 const initialValues = ({ number, iso }) => {
@@ -32,7 +29,7 @@ const initialValues = ({ number, iso }) => {
   }
   return {
     number: parsedNumber ?? '',
-    iso: { value: iso ?? '' },
+    iso: { value: iso ?? DEFAULT_COUNTRY_CODE, label: '' },
   };
 };
 
@@ -49,13 +46,11 @@ const PhoneNumberForm = React.forwardRef(
       confirmationDisabled,
       prefixLabelMessage,
       phoneLabel,
-      onError,
       allowPartial,
     },
     ref,
   ) => {
     const previousLoadingState = useRef(loading);
-    const inputNumberRef = useRef(null);
     const [modalVisible, setModalVisible] = useState(false);
     const openModal = () => setModalVisible(true);
     const closeModal = () => setModalVisible(false);
@@ -63,14 +58,14 @@ const PhoneNumberForm = React.forwardRef(
     const { number, iso, prefix, confirmed } = phone ?? {};
 
     const onSubmit = (
-      { number: submitNumber, iso: { value: isoValue } },
+      { number: submitNumber, iso: isoOption },
       { setSubmitting },
     ) => {
-      const prefixValue = `+${getCountryCallingCode(isoValue)}`;
-      const parsedNumber = parsePhoneNumber(submitNumber, isoValue);
+      const prefixValue = `+${getCountryCallingCode(isoOption?.value)}`;
+      const parsedNumber = parsePhoneNumber(submitNumber, isoOption?.value);
       const submitPayload = {
         number: parsedNumber?.nationalNumber ?? '',
-        iso: isoValue,
+        iso: isoOption?.value,
         prefix: prefixValue,
       };
       const hasPhoneNumberChanged = !isEqual(
@@ -96,30 +91,6 @@ const PhoneNumberForm = React.forwardRef(
       previousLoadingState.current = loading;
     }, [loading]);
 
-    const getCodeLabel = (country = 'US') => (
-      <Row align="center">
-        <FlagIcon code={country} />
-        <Text ml={10} fontSize={18}>{`${country} +${getCountryCallingCode(
-          country,
-        )}`}</Text>
-      </Row>
-    );
-
-    const prefixOptions = useMemo(
-      () =>
-        union(popularPrefixes, getCountriesCodes()).map((country) => ({
-          value: country,
-          label: getCodeLabel(country),
-          filterData: `${country} +${getCountryCallingCode(country)}`,
-        })),
-      [],
-    );
-
-    const filterOption = ({ data: { filterData } }, value) => {
-      if (!value) return true;
-      return filterData.toUpperCase().includes(value.toUpperCase());
-    };
-
     const shouldDisplayConfirmationButton =
       !confirmationDisabled &&
       !confirmed &&
@@ -130,36 +101,20 @@ const PhoneNumberForm = React.forwardRef(
       <Column>
         {error && <ErrorAlert mt={25} errorText={error} />}
         <Formik
-          validate={(values) => {
-            const {
-              iso: { value: country },
-            } = values;
-            const schema = phoneNumberSchema(
-              formatMessage,
-              country,
-              required,
-              allowPartial,
-            );
-            try {
-              validateYupSchema(values, schema, true);
-            } catch (err) {
-              if (onError) onError(err);
-              return yupToFormErrors(err);
-            }
-          }}
+          validationSchema={phoneNumberSchema(
+            formatMessage,
+            required,
+            allowPartial,
+          )}
           initialValues={initialValues({ number, iso })}
           onSubmit={onSubmit}
           innerRef={ref}
         >
-          {({
-            handleSubmit,
-            isValid,
-            values: { number: numberValue, iso: isoValue },
-          }) => {
+          {({ isValid, values: { number: numberValue, iso: isoOption } }) => {
             const isButtonActive = !isValid;
 
-            const currentPhoneNumber = isoValue?.value
-              ? ` +${getCountryCallingCode(isoValue.value)} ${numberValue}`
+            const currentPhoneNumber = isoOption?.value
+              ? ` +${getCountryCallingCode(isoOption.value)} ${numberValue}`
               : '';
 
             return (
@@ -170,42 +125,13 @@ const PhoneNumberForm = React.forwardRef(
                   phone={currentPhoneNumber}
                 />
                 <Row width="100%" align="start" data-private>
-                  <FormikSelect
-                    columnStyleProps={{
-                      pr: 10,
-                      width: 230,
-                    }}
+                  <FormikPhoneNumberInput
+                    isoKey="iso"
+                    numberKey="number"
                     disabled={disabled}
-                    label={formatMessage(prefixLabelMessage)}
-                    formikKey="iso"
-                    options={prefixOptions}
-                    inputProps={{
-                      filterOption,
-                      placeholder: formatMessage(messages.countryCode),
-                      onMenuClose: () => inputNumberRef.current.focus(),
-                      value: isoValue.value
-                        ? {
-                            value: isoValue.value,
-                            label: getCodeLabel(isoValue.value),
-                          }
-                        : null,
-                      disabled,
-                    }}
-                  />
-                  <FormikNumberInput
-                    label={formatMessage(phoneLabel)}
-                    value={numberValue}
-                    formikKey="number"
-                    placeholder={formatMessage(messages.phoneNumber)}
-                    type="tel"
-                    countryCode={isoValue.value}
-                    inputProps={{
-                      ref: inputNumberRef,
-                      width: '100%',
-                      onBlur: handleSubmit,
-                      disabled,
-                    }}
-                    required={required}
+                    submitOnChange
+                    prefixLabel={prefixLabelMessage}
+                    phoneLabel={phoneLabel}
                   />
                 </Row>
 
@@ -242,7 +168,6 @@ PhoneNumberForm.propTypes = {
   required: PropTypes.bool,
   prefixLabelMessage: PropTypes.object,
   phoneLabel: PropTypes.object,
-  onError: PropTypes.func,
   allowPartial: PropTypes.bool,
 };
 
