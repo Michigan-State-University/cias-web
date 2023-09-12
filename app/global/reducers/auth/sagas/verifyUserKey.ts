18b05c76e91401b153f3b7fb8ae64ef4a13e9d58
bartosz-kepka-htd <bartosz.kepka@htdevelopers.com>
2023-09-12 10:52:33 +0200

    [CIAS30-3650] Remove mock

--- app/global/reducers/auth/sagas/verifyUserKey.ts
+++ app/global/reducers/auth/sagas/verifyUserKey.ts
@@ -1,12 +1,12 @@
 import { call, put, takeEvery } from '@redux-saga/core/effects';
 import { replace } from 'connected-react-router';
-
-import { Roles } from 'models/User/RolesManager';
+import axios from 'axios';
 
 import objectToSnakeCase from 'utils/objectToSnakeCase';
 import { parametrizeRoutePath } from 'utils/router';
 import { mapPlainUserData } from 'utils/mapResponseObjects';
 import LocalStorageService from 'utils/localStorageService';
+import objectToCamelCase from 'utils/objectToCamelCase';
 
 import { RoutePath } from 'global/constants';
 import { WithSaga } from 'global/reducers/types';
@@ -20,41 +20,6 @@ import {
 } from '../actions';
 import { VerifyUserKeyResponse } from '../types';
 
-const mockData = (): VerifyUserKeyResponse => {
-  return {
-    redirectData: {
-      userInterventionId: '9f3ba52d-2b8d-4ff8-9579-b4b02d5d59e9',
-      interventionId: 'e0c37422-551e-46c2-bcbd-b7301a234250',
-      // sessionId: '513edd15-3f96-45de-90d6-ce7b25f79b20',
-      sessionId: null,
-      healthClinicId: null,
-      multipleFillSessionAvailable: true,
-    },
-    user: {
-      id: 'f53117a2-b7d8-4cd3-acfc-f842e985fe48',
-      email: 'participant@interventionauthoring.org',
-      fullName: '',
-      firstName: '',
-      lastName: '',
-      description: '',
-      smsNotification: true,
-      timeZone: 'America/New_York',
-      active: true,
-      roles: [Roles.PredefinedParticipant],
-      phone: null,
-      teamId: null,
-      adminsTeamIds: [],
-      feedbackCompleted: false,
-      emailNotification: true,
-      organizableId: null,
-      quickExitEnabled: false,
-      teamName: null,
-      healthClinicsIds: null,
-      avatarUrl: null,
-    },
-  };
-};
-
 function* verifyUserKeyWorker({
   payload: { userKey },
 }: ReturnType<typeof verifyUserKeyRequest>) {
@@ -62,11 +27,9 @@ function* verifyUserKeyWorker({
   const requestBody = objectToSnakeCase({ userKey });
 
   try {
-    // const { data } = yield call(axios.post, requestUrl, requestBody);
-    // const { redirectData, user }: VerifyUserKeyResponse =
-    //   objectToCamelCase(data);
-    // TODO replace below mock with the above response data
-    const { redirectData, user } = mockData();
+    const { data } = yield call(axios.post, requestUrl, requestBody);
+    const { redirectData, user }: VerifyUserKeyResponse =
+      objectToCamelCase(data);
 
     const mappedUser = mapPlainUserData(user);
     yield call(LocalStorageService.setState, { user: mappedUser });
