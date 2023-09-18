3806f1935a1d28347033c9d7aa257348feaa19de
bartosz-kepka-htd <bartosz.kepka@htdevelopers.com>
2023-09-18 13:00:32 +0200

    [CIAS30-3631] Modal size and title

--- app/containers/InterventionDetailsPage/containers/InviteParticipantsButton/InviteParticipantsModal.tsx
+++ app/containers/InterventionDetailsPage/containers/InviteParticipantsButton/InviteParticipantsModal.tsx
@@ -5,6 +5,10 @@ import Modal, { Props as ModalComponentProps } from 'components/Modal/Modal';
 import { ShareBox, ShareBoxType } from 'containers/ShareBox';
 import { useIntl } from 'react-intl';
 import messages from './messages';
+import {
+  INVITE_PARTICIPANTS_MODAL_HEIGHT,
+  INVITE_PARTICIPANTS_MODAL_WIDTH,
+} from './constants';
 
 export type Props = {
   organizationId: Nullable<string>;
@@ -22,6 +26,14 @@ export const InviteParticipantsModal: FC<Props> = ({
       visible={visible}
       onClose={onClose}
       title={formatMessage(messages.inviteParticipantsModalTitle)}
+      width={INVITE_PARTICIPANTS_MODAL_WIDTH}
+      height={INVITE_PARTICIPANTS_MODAL_HEIGHT}
+      pt={24}
+      px={24}
+      pb={16}
+      titleProps={{
+        lineHeight: '36px',
+      }}
     >
       <ShareBox
         type={ShareBoxType.INTERVENTION}
