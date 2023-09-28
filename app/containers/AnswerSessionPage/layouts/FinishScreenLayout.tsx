6efa701cf8983a7e99dc65e326c915cd1e5675bc
bartosz-kepka-htd <bartosz.kepka@htdevelopers.com>
2023-09-28 10:13:41 +0200

    [CIAS30-3705] Remove todo

--- app/containers/AnswerSessionPage/layouts/FinishScreenLayout.tsx
+++ app/containers/AnswerSessionPage/layouts/FinishScreenLayout.tsx
@@ -138,7 +138,6 @@ const FinishScreenLayout = ({ formatMessage, question }: Props) => {
           </Button>
         </StyledLink>
       )}
-      {/* TODO https://htdevelopers.atlassian.net/browse/CIAS30-3705 redirect predefined user to USER_INTERVENTION page */}
       <StyledLink to={getGoToDashboardButtonLink()}>
         <Button
           onClick={isPreview ? closeCurrentTab : undefined}
