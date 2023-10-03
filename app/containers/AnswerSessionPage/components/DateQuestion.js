5c10b26571ca3a0b9c022dd5e52320e2565ee8de
bartosz-kepka-htd <bartosz.kepka@htdevelopers.com>
2023-10-03 15:52:47 +0200

    [CIAS30-3724] Disable date question answers when saving answers

--- app/containers/AnswerSessionPage/components/DateQuestion.js
+++ app/containers/AnswerSessionPage/components/DateQuestion.js
@@ -5,7 +5,12 @@ import isNullOrUndefined from 'utils/isNullOrUndefined';
 
 import DateQuestionLayout from '../layouts/DateQuestionLayout';
 
-const DateQuestion = ({ question, answerBody, selectAnswer }) => {
+const DateQuestion = ({
+  question,
+  answerBody,
+  selectAnswer,
+  selectingAnswersDisabled,
+}) => {
   const {
     body: {
       variable: { name },
@@ -24,7 +29,11 @@ const DateQuestion = ({ question, answerBody, selectAnswer }) => {
   };
 
   return (
-    <DateQuestionLayout onChange={onChange} answerBody={answerBody?.[0]} />
+    <DateQuestionLayout
+      onChange={onChange}
+      answerBody={answerBody?.[0]}
+      disabled={selectingAnswersDisabled}
+    />
   );
 };
 
@@ -32,6 +41,7 @@ DateQuestion.propTypes = {
   question: PropTypes.object.isRequired,
   selectAnswer: PropTypes.func,
   answerBody: PropTypes.any,
+  selectingAnswersDisabled: PropTypes.bool,
 };
 
 export default DateQuestion;
