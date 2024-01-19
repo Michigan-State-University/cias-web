b879fac1c735eaf56140f4042ee1d32dd49b8e14
bartosz-kepka-htd <bartosz.kepka@htdevelopers.com>
2024-01-19 15:28:26 +0100

    [CIAS30-3876] Don't require english locale in configureDayjs.ts

--- app/utils/dayjs/configureDayjs.ts
+++ app/utils/dayjs/configureDayjs.ts
@@ -13,18 +13,8 @@ import { DEFAULT_LOCALE } from 'i18n';
 
 import { CustomLocalesConfig, RELATIVE_TIME_OPTIONS } from './constants';
 
-require(`dayjs/locale/en`);
 require(`dayjs/locale/ar`);
 require(`dayjs/locale/es`);
-
-// appLocales.forEach((locale) => {
-//   try {
-//     // See dayjs docs https://day.js.org/docs/en/i18n/i18n for a list of supported languages
-//     // eslint-disable-next-line global-require
-//     require(`dayjs/locale/${locale}.js`);
-//     // eslint-disable-next-line no-empty
-//   } catch {}
-// });
 
 export const configureDayjs = () => {
   // Load plugins
