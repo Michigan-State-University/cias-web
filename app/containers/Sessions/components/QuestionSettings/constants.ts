3b40cf8da0a4a7a83f3aeaa9138300f60efa7142
krzepecki <50991414@users.noreply.github.com>
2024-04-15 09:51:19 +0200

    CIAS30-3782: hide settings and narrator tabs in question settings

--- app/containers/Sessions/components/QuestionSettings/constants.ts
+++ app/containers/Sessions/components/QuestionSettings/constants.ts
@@ -1,8 +1,14 @@
 import { QuestionTypes } from 'models/Question';
 
-export const HIDE_SETTINGS_TAB_QUESTIONS: QuestionTypes[] = [];
+export const HIDE_SETTINGS_TAB_QUESTIONS: QuestionTypes[] = [
+  QuestionTypes.SMS_QUESTION,
+  QuestionTypes.SMS_INFORMATION_QUESTION,
+];
 
-export const HIDE_NARRATOR_TAB_QUESTIONS: QuestionTypes[] = [];
+export const HIDE_NARRATOR_TAB_QUESTIONS: QuestionTypes[] = [
+  QuestionTypes.SMS_QUESTION,
+  QuestionTypes.SMS_INFORMATION_QUESTION,
+];
 
 export const HIDE_BRANCHING_TAB_QUESTIONS: QuestionTypes[] = [
   QuestionTypes.FINISH,
