927fde318605be7e10bdead9697ae9aa8b729049
WiktoriaRozanska <61981027+WiktoriaRozanska@users.noreply.github.com>
2025-12-19 13:56:05 +0100

    fix issu with automatically parsing to UTC (#340)

--- app/global/reducers/intervention/sagas/sendInterventionCsv.js
+++ app/global/reducers/intervention/sagas/sendInterventionCsv.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 import { put, takeLatest, call } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import get from 'lodash/get';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 
 import { SEND_INTERVENTION_CSV_REQUEST } from 'global/reducers/intervention/constants';
 import { formatMessage } from 'utils/intlOutsideReact';
@@ -12,20 +15,31 @@ import {
 } from '../actions';
 import messages from '../messages';
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 export function* sendInterventionCsv({
-  payload: { id, onSuccess, startDate, endDate, timezone },
+  payload: { id, onSuccess, startDate, endDate, timezone: selectedTimezone },
 }) {
   let requestURL = `v1/interventions/${id}/answers.csv`;
 
   const params = new URLSearchParams();
   if (startDate) {
-    params.append('start_datetime', startDate.toISOString());
+    const formattedStartDate = selectedTimezone
+      ? dayjs(startDate)
+          .tz(selectedTimezone, true)
+          .format('YYYY-MM-DDTHH:mm:ss')
+      : dayjs(startDate).format('YYYY-MM-DDTHH:mm:ss');
+    params.append('start_datetime', formattedStartDate);
   }
   if (endDate) {
-    params.append('end_datetime', endDate.toISOString());
+    const formattedEndDate = selectedTimezone
+      ? dayjs(endDate).tz(selectedTimezone, true).format('YYYY-MM-DDTHH:mm:ss')
+      : dayjs(endDate).format('YYYY-MM-DDTHH:mm:ss');
+    params.append('end_datetime', formattedEndDate);
   }
-  if (timezone) {
-    params.append('timezone', timezone);
+  if (selectedTimezone) {
+    params.append('timezone', selectedTimezone);
   }
 
   const queryString = params.toString();
